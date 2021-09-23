510375f5a9ace618743ece0cd7322999dc7ecf19
Peter O <pjo_6493@hotmail.com>
2021-09-22 20:52:14 -0400

    Update update_hama_and_ass.sh

--- update_hama_and_ass.sh
+++ update_hama_and_ass.sh
@@ -5,6 +5,9 @@
 
 # Requires plex already be insalled with Hama.bundle plug-in and git.
 
+# sample cron:
+# 0 6 * * 1 '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/update_hama_and_ass.sh' &> "/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/hama_and_ass_cron_update-$(date +'%Y%m%d-%H%M%S').log"
+
 service plexmediaserver stop
 cd '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-ins/Hama.bundle'
 git reset && git pull
