2b8795b37e873f3f6995d8a727cf2eb8ed05ebf8
Peter O <pjo_6493@hotmail.com>
2021-09-22 20:03:38 -0400

    Create install_hama_with_ass.sh

---
+++ install_hama_with_ass.sh
@@ -0,0 +1,17 @@
+# A script to install Hama.bundle plug-ing with Absolute-Series-Scanner
+# Hama.bundle: https://github.com/ZeroQI/Hama.bundle
+# ASS: https://github.com/ZeroQI/Absolute-Series-Scanner
+
+# Requires plex already be insalled and git.
+
+service plexmediaserver stop
+cd '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-ins'
+git clone https://github.com/ZeroQI/Hama.bundle.git
+chown -R plex:plex '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-ins/Hama.bundle'
+chmod 775 -R '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Plug-ins/Hama.bundle'
+
+mkdir -p '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Scanners/Series'
+wget -O '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Scanners/Series/Absolute Series Scanner.py' https://raw.githubu>
+chown -R plex:plex '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Scanners'
+chmod 775 -R '/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Scanners'
+service plexmediaserver restart
